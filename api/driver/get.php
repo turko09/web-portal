d26d150f64fa60df5daf788b98e2cfc7fc1852ce
Angelito Sardez, Jr <jojosardez@gmail.com>
2018-03-26 00:12:59 +1100

    Driver APIs register and get implementation and introduced config, and db and http utility classes.

--- api/driver/get.php
+++ api/driver/get.php
@@ -1,18 +1,21 @@
 <?php
 namespace TeamAlpha\Web;
 
-// HTTP headers for response
-header('Access-Control-Allow-Orgin: *');
-header("Access-Control-Allow-Methods: GET");
-header('Content-Type: application/json; charset=UTF-8');
-
+// Require classes
+require $_SERVER['DOCUMENT_ROOT'] . '/api/utils/db.php';
+require $_SERVER['DOCUMENT_ROOT'] . '/api/utils/http.php';
 require $_SERVER['DOCUMENT_ROOT'] . '/api/models/driver.php';
+
+// Declare use on objects to be used
+use Exception;
+use PDOException;
+
+// HTTP headers for response
+Http::SetDefaultHeaders('GET');
 
 // Check if request method is correct
 if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
-    // Reply with error response
-    header('HTTP/1.1 405 Method Not Allowed');
-    echo json_encode(array('message' => 'Request method is not allowed.'));
+    Http::ReturnError(405, array('message' => 'Request method is not allowed.'));
     return;
 }
 
@@ -23,70 +26,49 @@ if (array_key_exists('id', $_GET)) {
     $id = intval($_GET['id']);
 }
 
-if ($id === 0) {
-    // Id was not given
-    // Return all drivers
+try {
+    if ($id === 0) {
+        // Id was not given
+        // Return all drivers
 
-    // TO DO: Return of actual drivers
+        // Create Db object
+        $db = new Db('SELECT * FROM `driver`');
 
-    $response = array();
+        $response = array();
 
-    $driver1 = new Driver();
-    $driver1->id = 1;
-    $driver1->firstname = 'Juan';
-    $driver1->lastname = 'Dela Cruz';
-    $driver1->email = 'juan@delacruz.com';
-    $driver1->password = 'hidden';
-    $driver1->address = 'Manila, Philippines';
-    $driver1->mobile = '09200000000';
-    array_push($response, $driver1);
-
-    $driver2 = new Driver();
-    $driver2->id = 2;
-    $driver2->firstname = 'John';
-    $driver2->lastname = 'Doe';
-    $driver2->email = 'john@doe.com';
-    $driver2->password = 'hidden';
-    $driver2->address = 'Laguna, Philippines';
-    $driver2->mobile = '09211111111';
-    array_push($response, $driver2);
-
-    $driver3 = new Driver();
-    $driver3->id = 3;
-    $driver3->firstname = 'Joe';
-    $driver3->lastname = 'Bloggs';
-    $driver3->email = 'joe@bloggs.com';
-    $driver3->password = 'hidden';
-    $driver3->address = 'Taguig, Philippines';
-    $driver3->mobile = '09222222222';
-    array_push($response, $driver3);
-
-    // Reply with successul response
-    header('HTTP/1.1 200 OK');
-    echo json_encode($response);
-} else {
-    // TO DO: Actual check if driver exists
-    if ($id === 404) {
-        // Sample not found
-        // Reply with error response
-        header('HTTP/1.1 404 Not Found');
-        echo json_encode(array('message' => 'Driver not found.'));
-    } else {
-        // Driver was found
-
-        // TO DO: Return of actual driver
-
-        $driver = new Driver();
-        $driver->id = $id;
-        $driver->firstname = 'Juan';
-        $driver->lastname = 'Dela Cruz';
-        $driver->email = 'juan@delacruz.com';
-        $driver->password = 'hidden';
-        $driver->address = 'Manila, Philippines';
-        $driver->mobile = '09200000000';
+        // Execute
+        if ($db->execute() > 0) {
+            // Drivers were found
+            $records = $db->fetchAll();
+            foreach ($records as &$record) {
+                $driver = new Driver($record);
+                array_push($response, $driver);
+            }
+        }
 
         // Reply with successful response
-        header('HTTP/1.1 200 OK');
-        echo json_encode($driver);
+        Http::ReturnSuccess($response);
+    } else {
+        // Create Db object
+        $db = new Db('SELECT * FROM `driver` WHERE id = :id LIMIT 1');
+
+        // Bind parameters
+        $db->bindParam(':id', $id);
+
+        // Execute
+        if ($db->execute() === 0) {
+            Http::ReturnError(404, array('message' => 'Driver not found.'));
+        } else {
+            // Driver was found
+            $record = $db->fetchAll()[0];
+            $driver = new Driver($record);
+
+            // Reply with successful response
+            Http::ReturnSuccess($driver);
+        }
     }
+} catch (PDOException $pe) {
+    Db::ReturnDbError($pe);
+} catch (Exception $e) {
+    Http::ReturnError(500, array('message' => 'Server error: ' . $e->getMessage() . '.'));
 }
--- api/models/driver.php
+++ api/models/driver.php
@@ -7,7 +7,31 @@ class Driver
     public $firstname;
     public $lastname;
     public $email;
-    public $password;
     public $address;
     public $mobile;
+    public $active;
+    public $verified;
+    public $blocked;
+    public $photo;
+    public $datecreated;
+    public $datemodified;
+
+    public function __construct(array $data)
+    {
+        if ($data !== null) {
+            $this->id = (int) $data['id'] ?? 0;
+            $this->firstname = $data['firstname'] ?? null;
+            $this->lastname = $data['lastname'] ?? null;
+            $this->email = $data['email'] ?? null;
+            $this->address = $data['address'] ?? null;
+            $this->mobile = $data['mobile'] ?? null;
+            $this->active = (int) $data['active'] ?? 0;
+            $this->verified = (int) $data['verified'] ?? 0;
+            $this->blocked = (int) $data['blocked'] ?? 0;
+            $this->photo = $data['photo'] ?? null;
+            $this->datecreated = $data['datecreated'] ?? null;
+            $this->datemodified = $data['datemodified'] ?? null;
+        }
+    }
+
 }
